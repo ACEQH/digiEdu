555070e411d1f443ea4090d77e90d8eba0b3d9e6
motazalshehri <motazalshehri@gmail.com>
2020-03-19 02:42:43 +0300

    the name changes when you pick a file to upload in assignment

--- src/app/thome/assign-homework/assign-homework.component.ts
+++ src/app/thome/assign-homework/assign-homework.component.ts
@@ -4,9 +4,15 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-assign-homework',
   templateUrl: './assign-homework.component.html',
   styleUrls: ['./assign-homework.component.css']
+
 })
 export class AssignHomeworkComponent implements OnInit {
-
+  fileName: any;
+  fileProgress(fileInput: any) {
+    let file = fileInput.target.files[0];
+    this.fileName = file.name;
+    console.log(this.fileName)
+  }
   constructor() { }
 
   ngOnInit() {
